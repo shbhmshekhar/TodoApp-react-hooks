42968938dac559966565f86a29ba2f63e192575b
Shubham Shekhar <shbhmshekhar@outlook.com>
2021-09-01 11:12:53 +0530

    Todo App using function based component and React Hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Todos, setTodos] = useState([]);
+
+  let userIpTodoUI = '';
+
+  const ipChangeHandler = (event) => {
+    userIpTodoUI = event.target.value;
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (userIpTodoUI !== null || userIpTodoUI !== '') {
+      const newTodo = {
+        id: Math.random(),
+        todoTask: userIpTodoUI,
+        value: Todos.todoTask,
+      };
+
+      setTodos([...Todos, newTodo]);
+      console.log(Todos);
+    }
+
+    userIpTodoUI = null;
+  };
+
+  const delTodo = (id) => {
+    const remainigTodo = Todos.filter((todo) => todo.id !== id);
+    setTodos(remainigTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          onChange={ipChangeHandler}
+          placeholder='Enter Todo Here'
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
+      {Todos &&
+        Todos.map((todo) => (
+          <p key={todo.id}>
+            {todo.todoTask}
+            <button type='button' onClick={() => delTodo(todo.id)}>
+              X
+            </button>
+          </p>
+        ))}
     </div>
   );
 }
